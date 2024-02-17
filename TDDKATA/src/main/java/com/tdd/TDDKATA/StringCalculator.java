9effaab71648dfa60d90a889e63444da29547e8b
kamlesh123git <kdns1810@gmail.com>
2024-02-17 12:56:14 +0530

    Test case for new line

--- TDDKATA/src/main/java/com/tdd/TDDKATA/StringCalculator.java
+++ TDDKATA/src/main/java/com/tdd/TDDKATA/StringCalculator.java
@@ -1,27 +1,35 @@
 package com.tdd.TDDKATA;
 
 public class StringCalculator {
-	
-	
-    int add(String numbers) {
-    	int result = 0;
-    	if(numbers.isEmpty() || numbers.isBlank()) {
-    		return 0;
-    	}
-    	String [] arr = numbers.split(",");
-    	
-    	if(arr.length == 1) {
-    		return Integer.parseInt(arr[0]);
-    	}
+
+	int add(String numbers) {
+		int result = 0;
+		String[] arr;
+		if (numbers.isEmpty() || numbers.isBlank()) {
+			return 0;
+		}
 		
-    	else {
-    		
-    		for( int i=0;i< arr.length; i++) {
-    			result = result + Integer.parseInt(arr[i]);
-    		}
-    		return result;
-    	}
+		if (numbers.contains("\n")) {
+			String numberWONewLIne= numbers.replace("\n", "");
+			System.out.println(numbers);
+			arr = numberWONewLIne.split(",");
+		}
+		else {
+		 arr = numbers.split(",");
+		}
+
 		
-    	
-    }
+		if (arr.length == 1) {
+			return Integer.parseInt(arr[0]);
+		}
+
+		else {
+
+			for (int i = 0; i < arr.length; i++) {
+				result = result + Integer.parseInt(arr[i]);
+			}
+			return result;
+		}
+
+	}
 }
--- TDDKATA/src/test/java/com/tdd/TDDKATA/AppTest.java
+++ TDDKATA/src/test/java/com/tdd/TDDKATA/AppTest.java
@@ -41,4 +41,25 @@ public class AppTest
         Assert.assertEquals(expected, sum);
        
     }
+    
+    @Test
+    public void findTheSumOfManyNumber()
+    {
+    	String numbers = "1,2,3,4,5,8,9";
+        int  sum = stringCal.add(numbers);
+        int expected = 32;
+        Assert.assertEquals(expected, sum);
+       
+    }
+    
+    @Test
+    public void findTheSumOfManyNumberHAvingNewLine()
+    {
+    	String numbers = "1,2\n,3,4,5,8,9";
+    	
+        int  sum = stringCal.add(numbers);
+        int expected = 32;
+        Assert.assertEquals(expected, sum);
+       
+    }
 }
